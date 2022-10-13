950bf253e88a6a58e1600262afaff733cd992e00
imusa007 <irajibdu@gmail.com>
2022-10-12 23:51:03 -0400

    added support for  SRO calculation for arbitary number of shell

--- QV/Analysis/sro.py
+++ QV/Analysis/sro.py
@@ -13,25 +13,36 @@ from ase.io.trajectory import Trajectory
 class SRO:
 
     def __init__(self, atoms,cutoffs) -> None:
+        cutoffs.sort(reverse=True)
         self.atoms=atoms
         self.cutoffs=cutoffs
         self.elements=list(self.atoms.symbols)
         self.atom_dict={k:v for k,v in enumerate(self.elements)}
-        self.ndata=self.get_neighbor_count()
+        self.ndatas=self.get_neighbor_count()
 
     def get_neighbor_count(self):
+        nth_shell_counts=[]
+        for cutoff in self.cutoffs:
+            i,j=neighbor_list('ij',self.atoms,cutoff)
+            data=defaultdict(list)
+            for p,q in zip(i,j):
+                data[self.atom_dict[p]].append(self.atom_dict[q])
+            nth_shell_counts.append({key:Counter(data[key]) for key in data.keys()})
         
-        i,j=neighbor_list('ij',self.atoms,self.cutoffs)
-        data=defaultdict(list)
-        for p,q in zip(i,j):
-            data[self.atom_dict[p]].append(self.atom_dict[q])
-        ndata={key:Counter(data[key]) for key in data.keys()}
-        return ndata
+        #print(len(nth_shell_counts))
 
-    def get_sro(self,A,B): 
-        R_A_random=Counter(self.elements)[A]/sum(Counter(self.elements).values())
-        R_B_random=Counter(self.elements)[B]/sum(Counter(self.elements).values())
-        R_A_mc=self.ndata[A][B]/sum(self.ndata[A].values())
-        sro=(1-R_A_mc/R_A_random)
-        return sro
+        for idx in range(len(nth_shell_counts)-1):
+            for key in nth_shell_counts[idx].keys():
+                nth_shell_counts[idx][key].subtract(nth_shell_counts[idx+1][key])
+        return nth_shell_counts
+    
+
+    def get_sro(self,A,B):
+        sros=[]
+        for shell_number in range(len(self.cutoffs)): 
+            R_A_random=Counter(self.elements)[A]/sum(Counter(self.elements).values())
+            R_B_random=Counter(self.elements)[B]/sum(Counter(self.elements).values())
+            R_A_mc=self.ndatas[shell_number][A][B]/sum(self.ndatas[shell_number][A].values())
+            sros.append((1-R_A_mc/R_A_random))
+        return sros
         
--- test/test_sro.py
+++ test/test_sro.py
@@ -8,13 +8,14 @@ class TestSRO(unittest.TestCase):
 
     def test_sro(self):
         atoms=read('CONTCAR-HEA1-T100')
-        sro_analyzer=SRO(atoms,cutoffs=2.90)
+        sro_analyzer=SRO(atoms,cutoffs=[3.60,2.90])
         
-        sro_WW=sro_analyzer.get_sro('W','W')
-        sro_TiTa=sro_analyzer.get_sro('Ti','Ta')
-        sro_TaCr=sro_analyzer.get_sro('Ta','Cr')
+        sros_WW=sro_analyzer.get_sro('W','W')
+        sros_TiTa=sro_analyzer.get_sro('Ti','Ta')
+        sros_TaCr=sro_analyzer.get_sro('Ta','Cr')
         
-        assert np.allclose([sro_WW,sro_TiTa,sro_TaCr], [0.03125, 0.25758, -0.93798], rtol=1e-03, atol=1e-03)
+        # print(sros_WW[1],sros_TiTa[1],sros_TaCr[1])
+        assert np.allclose([sros_WW[1],sros_TiTa[1],sros_TaCr[1]], [0.03125, 0.25758, -0.93798], rtol=1e-03, atol=1e-03)
 
 if __name__ == '__main__':
     unittest.main()
